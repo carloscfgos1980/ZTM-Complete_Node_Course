d275de0daf0efc96c0bce6e291bb2c18e7f3bd3f
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2024-04-01 16:43:52 +0200

    Update server.js
    codes in comments of previous lessons

--- 08.Improving Node performance/performance-example/server.js
+++ 08.Improving Node performance/performance-example/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+// const cluster = require('cluster');
+// const os = require('os');
 const app = express();
 
 function delay(duration){
@@ -14,19 +16,41 @@ app.get('/', (req, res)=>{
     // JSON.stringify({}) => "{}"
     //JSON.parse("{}") => {}
     // [4,5,6,7,1].sort();
-    res.send(`performance example ${process.pid}`)
+    res.send(`performance example: ${process.pid}`)
 });
 
 app.get('/timer', (req, res)=>{
     delay(9000)
-    res.send(`Ding ding ding ${process.pid}`)
+    res.send(`Ding ding ding: ${process.pid}`)
 });
+
+// console.log('running server...');
+
+// if(cluster.isPrimary){
+//     console.log('Master has been started...')
+//     cluster.fork();
+//     cluster.fork();
+// } else {
+//     console.log('Workers has started')
+//     app.listen(3000);
+// }
+
+
+// if(cluster.isMaster){
+// console.log('Master has been started');
+// const NUM_WORKERS = os.cpus().length;
+// for(let i = 0; i < NUM_WORKERS; i++){
+// cluster.fork();
+// }
+
+// } else{
+// console.log('Worker process started')
+// app.listen(3000);
+// }
 
 console.log('Running server.js...');
 console.log('Worker process started')
 app.listen(3000);
-
-
 
 
 
