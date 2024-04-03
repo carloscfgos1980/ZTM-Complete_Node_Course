defabfece53a18efa8c0bac6e3c13241a69f234c
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2024-04-03 12:10:43 +0200

    Update launches.test.js
    Fixing bug
    .get('/launches'). I had lunches instead

--- 09.Databases/nasa-project/server/src/routes/launches/launches.test.js
+++ 09.Databases/nasa-project/server/src/routes/launches/launches.test.js
@@ -17,7 +17,7 @@ describe('Launches API', ()=>{
     describe('Test GET / launches', ()=>{
         test('It should respond with 200 success', async ()=>{
             const response = await request(app)
-            .get('/lanches')
+            .get('/launches')
             .expect('Content-Type', /json/)
             .expect(200);
         });
